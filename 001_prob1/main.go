eb4a3507b51afa89c79a099a8c46d54da132bd06
Joey <joseph.woolever@yahoo.com>
2017-11-25 00:28:37 -0600

    typo edit

--- 001_prob1/main.go
+++ 001_prob1/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// If we list all the natural number below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23. What is the sum of all the multiples of 3 or 5 below 1000.
+// If we list all the natural number below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23. What is the sum of all the multiples of 3 or 5 below 1000?
 
 func main() {
 
